711988db4779512e29ad30d02dc3d27de2889aff
sangvorennlabs <sang.vo@rennlabs.com>
2025-07-16 11:36:20 +0700

    Update .gitignore, add README.md, remove README.txt, enhance chatbot message handling, refactor LLM services, and update requirements

--- back-end/services/llm_services.py
+++ back-end/services/llm_services.py
@@ -3,16 +3,13 @@ load_dotenv()
 
 import os
 from openai import OpenAI
+from langsmith.wrappers import wrap_openai
+
+client = wrap_openai(OpenAI(
+    api_key=os.getenv("OPENAI_API_KEY"),
+))
 
 def run_llm_chat_completion(system, messages, tools = []):
-    client = OpenAI(
-        api_key = os.getenv("OPENAI_API_KEY"),
-        base_url="https://oai.helicone.ai/v1",
-        default_headers= {  # Optionally set default headers or set per request (see below)
-            "Helicone-Auth": f"Bearer {os.getenv('HELICONE_API_KEY')}",
-        }
-    )
-    
     all_messages = [
         {
             "role": "system",
@@ -29,14 +26,6 @@ def run_llm_chat_completion(system, messages, tools = []):
     return response.choices[0].message
 
 def run_llm_structure_output(system, user_input, response_format):
-    client = OpenAI(
-        api_key = os.getenv("OPENAI_API_KEY"),
-        base_url="https://oai.helicone.ai/v1",
-        default_headers= {  # Optionally set default headers or set per request (see below)
-            "Helicone-Auth": f"Bearer {os.getenv('HELICONE_API_KEY')}",
-        }
-    )
-    
     completion = client.beta.chat.completions.parse(
         model="gpt-4o-mini",
         messages=[
@@ -49,8 +38,6 @@ def run_llm_structure_output(system, user_input, response_format):
     return completion.choices[0].message.parsed.model_dump()
 
 def get_word_embeddings(text):
-    client = OpenAI(api_key = os.getenv("OPENAI_API_KEY"))
-    
     response = client.embeddings.create(
         input=text,
         model="text-embedding-3-small"
